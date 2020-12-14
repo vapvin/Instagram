a3d888ec99602723343c91e091a568141f1c49e9
vapvin <vins@vins.dev>
2020-12-15 05:03:51 +0900

    ISSUES: IC-2, migrate user models

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ import { ApolloServer } from 'apollo-server-express';
 import logger from 'morgan';
 import helmet from 'helmet';
 import schema from './schema';
+import { sequelize } from '../database/models';
  
 const PORT = process.env.PORT || 4000;
 
@@ -13,4 +14,9 @@ const app = express();
 server.applyMiddleware({ app });
 app.use('*', logger('dev'), helmet);
  
-app.listen({ port: PORT }, () => console.log(`🚀 Server ready at http://localhost:4000/graphql <= this link `))
+app.listen({ port: PORT }, () => {
+  console.log(`🚀 Server ready at http://localhost:4000/graphql <= this link `);
+  sequelize.authenticate()
+    .then(() => console.log('Connected'))
+    .catch(err => console.log(err));
+});
