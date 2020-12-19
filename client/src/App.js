b0775148bb9f6b98708a9a10275668b4969a6468
hyemin065 <p62517@gmail.com>
2020-12-20 00:55:29 +0900

    IC-7, Register Test Code

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import GlobalStyles from './components/GlobalStyles';
 import Routers from './components/Routers';
+import ApolloProvider from './components/ApolloProvider';
 
 function App() {
   return (
-    <>
+    <ApolloProvider>
       <GlobalStyles />
       <Routers />
-    </>
+    </ApolloProvider>
   );
 }
 
--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -31,14 +31,14 @@ function Login(){
             </PhoneContainer>
             <InputnContainer>
                 <h1>
-                    <Link href="#">
+                    <Link to="#">
                         <img src="./img/logo.png" alt=""/>
                     </Link>
                 </h1>
                 <p>친구들의 사진과 동영상을 보려면 가입하세요.</p>
                 <JoinInput />
                 <div>
-                    <p>계정이 있으신가요? <Link href="#">로그인</Link></p>
+                    <p>계정이 있으신가요? <Link to="#">로그인</Link></p>
                 </div>
             </InputnContainer>
         </LoginContainer>
